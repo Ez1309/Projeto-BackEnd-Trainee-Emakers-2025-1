6446c58a903e368143ee0a88a3303f64c4fb8a57
Ezequiel Dominguez <eze.dmgz06@gmail.com>
2025-07-18 17:29:57 -0300

    add: commit inicial do spring initializr

---
+++ src/test/java/com/ezequiel/api_biblioteca/ApiBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezequiel.api_biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
